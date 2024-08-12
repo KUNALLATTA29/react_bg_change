e1d18b16d924a76d826df385ecb8e8e3506a63ee
kunal latta <kunalofficial234@gmail.com>
2024-08-12 12:33:12 +0530

    react bg change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
 import './App.css'
-import Card from './components/Card'
+import {useState} from 'react'
 
 function App() {
-
+  const [color, setColor] = useState("black");
   return (
-    <>
-      <Card username="kunal"/>
-      <Card username="lalu"/>
-    </>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}>
+        <div className="fixed item-center flex flex-wrap justify-center gap-3 ">
+          <button className='bg-red-500 w-2/4 text-white py-2 px-2 rounded'
+          onClick={()=> setColor("red")}>Red</button>
+          <button className='bg-green-500 w-2/4 text-white py-2 px-2 rounded'
+          onClick={()=> setColor("green")}>Green</button>
+          <button className='bg-black w-2/4 text-white py-2 px-2 rounded'
+          onClick={()=> setColor("black")}>Black</button>
+          <button className='bg-blue-500 w-2/4 text-white py-2 px-2 rounded'
+          onClick={()=> setColor("blue")}>Blue</button>
+          <button className='bg-purple-500 w-2/4 text-white py-2 px-2 rounded'
+          onClick={()=> setColor("purple")}>Purple</button>
+        </div>
+      </div>
   )
 }
 
